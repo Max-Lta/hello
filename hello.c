b9d5977f276b2a1fbbebc7eab7ffd5697cbe98ff
Nevann <alinecopeland@ONLY-8GB-KILL-ME.local>
2025-08-31 15:10:47 -0500

    added my name/major to this repo

--- hello.c
+++ hello.c
@@ -9,10 +9,11 @@
 
  int main(int argc, char **argv) {
 
-    printf("My name is Max");
-    printf("My major is Computer engineering");
-    printf("I have a minor in anthropology");
-
+    printf("My name is Max\n");
+    printf("My major is Computer engineering\n");
+    printf("I have a minor in anthropology\n");
+    printf("My name is Nevann!\n");
+    printf("My majors are Electrical Engineering and Statistics & Data Science.\n");
     return 0;
 
  }
